a41e4532953262f2cca48c862ca736b6dc68aa71
Suriya <suriya.bala.mari@gmail.com>
2026-04-14 11:51:07 +0530

    Commit

--- UserApplication/src/main/java/com/example/UserRepository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserRepository;
-
-import com.example.UserEntity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity,Integer> {
-}
